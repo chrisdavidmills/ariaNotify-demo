dddc623a81d9f9c5c6cd1f895b22d341907c7128
Chris Mills <chrisdavidmills@gmail.com>
2025-08-29 17:33:51 +0100

    Add code to repo

---
+++ index.js
@@ -0,0 +1,67 @@
+const form = document.querySelector("form");
+const item = document.querySelector("input[type='text']");
+const price = document.querySelector("input[type='number']");
+const priceList = document.querySelector("ul");
+const totalOutput = document.querySelector("p");
+
+let total = 0;
+
+function numberToSpokenPrice(numStr) {
+  const num2dp = String(Number(numStr).toFixed(2));
+  const arr = num2dp.split(".");
+  const poundNoun = Number(arr[0]) === 1 ? "pound" : "pounds";
+  if (arr[1] && Number(arr[1]) !== 0) {
+    return `${arr[0]} ${poundNoun} and ${Number(arr[1])} pence`;
+  } else {
+    return `${arr[0]} ${poundNoun}`;
+  }
+}
+
+function addItemToList(item, price) {
+  const listItem = document.createElement("li");
+  listItem.setAttribute("data-item", item);
+  listItem.setAttribute("data-price", price);
+  listItem.textContent = `${item}: £${Number(price).toFixed(2)}`;
+  const btn = document.createElement("button");
+  btn.textContent = `Remove ${item}`;
+
+  btn.addEventListener("click", (e) => {
+    const listItem = e.target.parentNode;
+    total -= Number(listItem.getAttribute("data-price"));
+    document.body.ariaNotify(`${listItem.getAttribute("data-item")} removed`, {
+      priority: "high",
+    });
+    updateTotal();
+    listItem.remove();
+  });
+
+  priceList.appendChild(listItem);
+  listItem.appendChild(btn);
+}
+
+function updateTotal() {
+  totalOutput.textContent = `Total: £${Number(total).toFixed(2)}`;
+  document.body.ariaNotify(`Total is now ${numberToSpokenPrice(total)}`, {
+    priority: "high",
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addItemToList(item.value, price.value);
+  total += Number(price.value);
+  updateTotal();
+
+  document.body.ariaNotify(
+    `Item ${item.value}, price ${numberToSpokenPrice(
+      price.value
+    )}, added to list`,
+    {
+      priority: "high",
+    }
+  );
+
+  item.value = "";
+  price.value = "";
+});
