ee1ce3ce36841da05a4b0e9b959eda39f4836f11
Chris Mills <chrisdavidmills@gmail.com>
2025-09-10 11:13:47 +0100

    remove spoken price output code as not needed

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ const totalOutput = document.querySelector("p");
 
 let total = 0;
 
-function numberToSpokenPrice(numStr) {
-  const num2dp = String(Number(numStr).toFixed(2));
-  const arr = num2dp.split(".");
-  const poundNoun = Number(arr[0]) === 1 ? "pound" : "pounds";
-  if (arr[1] && Number(arr[1]) !== 0) {
-    return `${arr[0]} ${poundNoun} and ${Number(arr[1])} pence`;
-  } else {
-    return `${arr[0]} ${poundNoun}`;
-  }
-}
-
 function addItemToList(item, price) {
   const listItem = document.createElement("li");
   listItem.setAttribute("data-item", item);
@@ -41,7 +30,7 @@ function addItemToList(item, price) {
 
 function updateTotal() {
   totalOutput.textContent = `Total: £${Number(total).toFixed(2)}`;
-  totalOutput.ariaNotify(`Total is now ${numberToSpokenPrice(total)}`, {
+  totalOutput.ariaNotify(`Total is now £${total}`, {
     priority: "high",
   });
 }
@@ -53,9 +42,7 @@ form.addEventListener("submit", (e) => {
   total += Number(price.value);
 
   totalOutput.ariaNotify(
-    `Item ${item.value}, price ${numberToSpokenPrice(
-      price.value
-    )}, added to list`,
+    `Item ${item.value}, price £${price.value}, added to list`,
     {
       priority: "high",
     }
